c91acc850bb4ee42398b20f4ff26e5bb91aa23f9
Luan <dev.luanoliveira@gmail.com>
2022-09-14 11:59:52 -0300

    Update

---
+++ chamar_senha/util/funcs.py
@@ -0,0 +1,73 @@
+from util.modules import *
+from util.models import *
+
+senha, senha_anterior,senha_default, atendimento, prioritario = (0,0,0,0,0)
+config = ConfigParser()
+get_dir = os.path.dirname(__file__)
+config.read(fr'{get_dir}\settings.ini')
+server_config = dict(config['server'])
+opcao = server_config['setor']
+
+class Functions():
+    #função para chamar senhas na tabela fila_espera
+    def chamar_proxima(self):
+        global data, opcao,senha_anterior,senha_default, senha, atendimento, prioritario
+        senha_anterior = senha_default
+        
+        sql = f'SELECT senha FROM fila_espera WHERE opcao = "{opcao}" AND status = "ESPERA";'
+        resultado = consulta_database(sql)
+        if resultado != None:
+            if prioritario < 2:
+                atendimento = 'preferencial'
+                prioritario += 1
+            elif prioritario ==2:
+                # a cada duas preferencial chama uma convencional
+                atendimento = 'convencional'
+                prioritario = 0
+
+            sql = f'SELECT senha FROM fila_espera WHERE id IN (SELECT MIN(id) FROM fila_espera WHERE opcao = "{opcao}" AND atendimento = "{atendimento}" AND status = "ESPERA");'
+            
+            senha = consulta_database(sql)
+            if senha == None:
+                self.chamar_proxima()
+            else:
+                for i in senha:
+                    senha = i['senha']
+
+                sql = f'UPDATE fila_espera SET status = "CHAMADA" WHERE senha = "{senha}"'
+                update_databe(sql)
+                data = [senha,opcao]
+                chamar_senha(data)
+                # display = senha
+                self.display_senha.config(text=str(senha))
+                senha_default = senha
+
+        else:
+            messagebox.showinfo('Atenção!','Não há senhas para chamar')
+
+    # função para chamar senha anterior
+    def chamar_anterior(self):
+        global senha_anterior
+        data = [senha_anterior,opcao]
+        chamar_senha(data)
+        self.display_senha.config(text=str(senha_anterior))
+    
+    #função para chamar novamente senha atual
+    def chamar_novamente(self):
+        global data
+        chamar_senha(data)
+
+    # atualização da barra de status
+    def update_statubar(self):
+        global opcao
+        sql = f'SELECT COUNT(senha) FROM fila_espera WHERE opcao="{opcao}" AND status = "ESPERA";'
+        consulta = consulta_database(sql)
+        if consulta != None:
+            for i in consulta:
+                espera = i['COUNT(senha)']
+            self.display_espera.config(text=espera)
+            self.display_espera.after(200, self.update_statubar)
+
+    def open_settings(self):
+        ConfigGUI()
+
---
+++ chamar_senha/util/settings.py
@@ -0,0 +1,131 @@
+from modules import *
+
+get_dir = os.getcwd()+r'\chamar_senha'
+icon = fr'{get_dir}\images\sfs.ico'
+config = ConfigParser()
+configFile = fr'{get_dir}\util\settings.ini'
+
+class Functionsconfig():
+    def salvar_config(self):
+        '''
+        # salvar configuração do arquivo .ini
+            ConfigParser - iterpretador/analisador do arquivo
+                .add_section('nome da seção definida')
+                .set('nome da seção definida','opção/chave','valor opção/chave')
+
+            with - garantir que os recursos serão finalizados
+                .write('arquivo de configuração passado no metodo with') - pertence ao ConfigParser
+
+        '''
+        global config, configFile
+        if messagebox.askyesno('Configurações','Tem certeza que deseja salvar estás alterações?') == YES:
+            config = ConfigParser()
+            config.add_section('server')
+            config.set('server','host', self.entry_servidor.get())
+            config.set('server','user', self.entry_username.get())
+            config.set('server','password', self.entry_pass.get())
+            config.set('server','db', self.entry_tabela.get())
+            config.set('server','setor', self.entry_setor.get())
+            with open(configFile,'w', encoding='UTF-8') as file:
+                config.write(file)
+            messagebox.showinfo('Configurações','Alterações salvas com sucesso!')
+            self.root.destroy()
+
+    def loadConfigFile(self):
+        '''
+        # carrega configuração do arquivo .ini em try:, caso o arquivo não exista gera um novo arquivo em except:
+        
+        .read('arquivo ConfigParser') #lê o arquivo
+        .dict('arquivo ConfigParser['seção definida']') #cria um dicionário para o interpretador
+        '''
+        global config, configFile
+        try:
+            with open(configFile,'r') as file:
+                file.close()
+        except IOError:
+            config['server'] = {
+                'host': 'localhost',
+                'user': 'sms',
+                'password': '',
+                'db': 'database_att',
+                'setor': ''
+            }
+            with open(configFile, 'w', encoding='UTF-8') as file:
+                config.write(file)
+
+        config.read(configFile)
+        server_config = dict(config['server'])
+        self.entry_servidor.insert(0,server_config['host'])
+        self.entry_username.insert(0,server_config['user'])
+        self.entry_tabela.insert(0,server_config['db'])
+        self.entry_setor.insert(0,server_config['setor'])
+        self.entry_pass.insert(0,server_config['password'])
+
+class ConfigGUI(Functionsconfig):
+    def __init__(self):
+        self.root = tk.Tk()
+        self.make_root()
+        self.make_frame()
+        self.make_widgets()
+        self.loadConfigFile()
+        self.root.mainloop()
+
+    def make_root(self):
+        width_root = 450
+        height_root = 300
+        width_screnn = self.root.winfo_screenwidth()
+        height_screnn = self.root.winfo_screenheight()
+        pos_x = width_screnn/2 - width_root/2
+        pos_y = height_screnn/2 - height_root/2
+        geometry_root = '%dx%d+%d+%d' % (width_root,height_root,pos_x,pos_y)
+        self.root.title('Configurações - chamar senha')
+        self.root.iconbitmap(default=icon)
+        self.root.geometry(geometry_root)
+        self.root.maxsize(width=width_screnn, height=height_screnn)
+        self.root.resizable(False, False)
+    
+    def make_frame(self):
+        self.frame = Frame(self.root)
+        self.frame.place(relx= 0.1,rely=0, relwidth=0.8, relheight=1)
+
+    def make_widgets(self):
+        self.label = Label(self.frame, text='Hostname:')
+        self.label.config(font=(0,12),anchor='w',)
+        self.label.place(relwidth=0.5, relheight=0.08, rely=0.1)
+        self.entry_servidor = Entry(self.frame)
+        self.entry_servidor.config(font=(0,12),justify='left')
+        self.entry_servidor.place(relwidth=0.5, relheight=0.08,relx=0.5, rely=0.1)
+
+        self.label = Label(self.frame, text='Username:')
+        self.label.config(font=(0,12),anchor='w',)
+        self.label.place(relwidth=0.5, relheight=0.08, rely=0.22)
+        self.entry_username = Entry(self.frame)
+        self.entry_username.config(font=(0,12),justify='left')
+        self.entry_username.place(relwidth=0.5, relheight=0.08,relx=0.5, rely=0.22)
+
+        self.label = Label(self.frame, text='Database:')
+        self.label.config(font=(0,12),anchor='w',)
+        self.label.place(relwidth=0.5, relheight=0.08, rely=0.34)
+        self.entry_tabela = Entry(self.frame)
+        self.entry_tabela.config(font=(0,12),justify='left')
+        self.entry_tabela.place(relwidth=0.5, relheight=0.08,relx=0.5, rely=0.34)
+
+        self.label = Label(self.frame, text='Setor:')
+        self.label.config(font=(0,12),anchor='w',)
+        self.label.place(relwidth=0.5, relheight=0.08, rely=0.46)
+        self.entry_setor = Entry(self.frame)
+        self.entry_setor.config(font=(0,12),justify='left')
+        self.entry_setor.place(relwidth=0.5, relheight=0.08,relx=0.5, rely=0.46)
+
+        self.label = Label(self.frame, text='Password:')
+        self.label.config(font=(0,12),anchor='w',)
+        self.label.place(relwidth=0.5, relheight=0.08, rely=0.58)
+        self.entry_pass = Entry(self.frame)
+        self.entry_pass.config(font=(0,12),justify='left', show='*')
+        self.entry_pass.place(relwidth=0.5, relheight=0.08,relx=0.5, rely=0.58)
+
+        self.btn_salvar = Button(self.frame, text='Salvar')
+        self.btn_salvar.place(relwidth=0.5, relheight=0.12,relx=0.5, rely=0.76)
+        self.btn_salvar.config(cursor='hand2', command=self.salvar_config)
+
+ConfigGUI()
