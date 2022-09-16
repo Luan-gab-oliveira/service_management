eb4b474e37cc0d3d185d08f45b95b3589094a75f
Luan <dev.luanoliveira@gmail.com>
2022-09-16 12:01:18 -0300

    Update

--- painel/dist/app/modules.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from tkinter import messagebox
-from pathlib import Path
-from datetime import datetime
-from playsound import playsound
-from configparser import ConfigParser
-import tkinter as tk
-import threading,socket, os, gtts
-
